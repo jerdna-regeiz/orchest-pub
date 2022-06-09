dd72ce614b18ff49a01e6a63c5daee6af7de2c78
Bruno <bruno@orchest.io>
2022-06-09 14:26:42 +0100

    Tweak copy of Project delete dialog

--- services/orchest-webserver/client/src/views/ProjectSettingsView.tsx
+++ services/orchest-webserver/client/src/views/ProjectSettingsView.tsx
@@ -19,7 +19,7 @@ import {
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import DeleteIcon from "@mui/icons-material/Delete";
 import SaveIcon from "@mui/icons-material/Save";
-import { Dialog } from "@mui/material";
+import { Dialog, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import LinearProgress from "@mui/material/LinearProgress";
@@ -141,15 +141,16 @@ const ProjectSettingsView: React.FC = () => {
       return;
     }
     setConfirm(
-      "Warning",
-      "Are you certain that you want to delete this project? This will kill all associated resources and also delete all corresponding jobs. (This cannot be undone.)",
+      `Delete ${state.path}?`,
+      "Warning: Deleting a Project is permanent. All associated Jobs and resources will be deleted and unrecoverable.",
       async (resolve) => {
+        setIsDeletingProject(true);
         try {
+          await deleteProject(projectUuid);
           if (projectsState.projectUuid === projectUuid) {
             dispatch({ type: "SET_PROJECT", payload: undefined });
           }
 
-          await deleteProject(projectUuid);
           navigateTo(siteMap.projects.path);
         } catch (error) {
           setAlert(
@@ -161,7 +162,7 @@ const ProjectSettingsView: React.FC = () => {
           );
         } finally {
           resolve(true);
-          setIsDeletingProject(true);
+          setIsDeletingProject(false);
           return true;
         }
       },
@@ -184,17 +185,10 @@ const ProjectSettingsView: React.FC = () => {
       <Dialog open={isDeletingProject}>
         <Box
           sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            width: "20%",
-            bgcolor: "background.paper",
-            boxShadow: 24,
             p: 4,
           }}
         >
-          <div>Deleting project...</div>
+          <Typography variant="h5">Deleting project...</Typography>
           <Box sx={{ marginTop: 4 }}>
             <LinearProgress />
           </Box>
